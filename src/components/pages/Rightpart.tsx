b68959a353b1cc4a78d7a7e44e2c2732514a1b98
Gakiri001 <gakirijulius@gmail.com>
2025-05-10 13:22:21 +0300

    Refactor Rightpart component layout and enhance weather display with icons(Part 2)

--- src/components/pages/Rightpart.tsx
+++ src/components/pages/Rightpart.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
+import { FaCloudRain } from "react-icons/fa6";
+import { IoSunnyOutline } from "react-icons/io5";
+import { CiCloudOn } from "react-icons/ci";
 
 type Props = {}
 
@@ -8,21 +11,36 @@ export default function Rightpart({}: Props) {
   return (
      <div className='w-[70%]'>
         <div className='flex items-center justify-between p-4'>
-            <div className='flex items-center justify-between w-[70%]'>
-                <Input className='mr-1 border-1 border-gray-300' placeholder='Search Area'/>
+            <div className='flex items-center justify-between w-[70%] gap-x-1'>
+                <Input className=' border-1 border-gray-300' placeholder='Search Area'/>
                 <Button className='cursor-pointer'>Go</Button>
             </div>
-            <div className='w-[20%]'>
+            <div className='w-[20%] gap-x-1 flex justify-end'>
                 <Button className='cursor-pointer'>ºC</Button> 
                 <Button variant="ghost" className='border-2 border-gray-300 cursor-pointer'>ºF</Button>
+            </div>
+        </div>
+        <div className='flex items-center justify-between p-4 gap-2'>
+            <div className='flex items-center flex-col p-4 justify-center border-1 border-gray-400 rounded-[10px] w-[90%] h-[80%]'>
+                <p>22 May 2025</p>
+                <FaCloudRain className='text-[100px] text-blue-500 m-4'/>
+                <p className='text-1xl font-bold'>13-15 ºC</p>
+            </div>
+            <div className='flex items-center flex-col p-4 justify-center border-1 border-gray-400 rounded-[10px] w-[90%] h-[80%]'>
+                <p>23 May 2025</p>
+                <IoSunnyOutline className='text-[100px] text-[#FFB800] m-4'/>
+                <p className='text-1xl font-bold'>19-22 ºC</p>
+            </div>
+            <div className='flex items-center flex-col p-4 justify-center border-1 border-gray-400 rounded-[10px] w-[90%] h-[80%]'>
+                <p>24 May 2025</p>
+                <CiCloudOn className='text-[100px] text-blue-200 m-4'/>
+                <p className='text-1xl font-bold'>15-17-15 ºC</p>
             </div>
         </div>
         <div>
             <div></div>
             <div></div>
-            <div></div>
         </div>
-        <div></div>
      </div>
   )
 }
